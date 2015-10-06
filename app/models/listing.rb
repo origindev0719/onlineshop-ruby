6a30fa0ea440b4c82ca6e76089a327637852f772
origindev0719 <origindev0719@gmail.com>
2015-10-06 16:05:10 +0000

    Validate user and listing data

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ActiveRecord::Base
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: {greater_than: 0}
+  validates_attachment_presence :image
 end
