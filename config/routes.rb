0526cadde4d8173f1d1964698164bc0a3b444004
origindev0719 <origindev0719@gmail.com>
2015-10-27 19:01:23 +0000

    Add sales and purchases routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :users
   
   get 'seller' =>"listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
   
   root 'listings#index'
   # The priority is based upon order of creation: first created -> highest priority.
